3dd41724c55a4d85939d735c2edbdd57b504e9c6
Admin <Admin@DESKTOP-UU4ITKA>
2026-07-16 10:29:00 +0530

    New Try

--- src/test/java/Demo/Practice.java
+++ src/test/java/Demo/Practice.java
@@ -2,6 +2,7 @@ package Demo;
 
 public class Practice {
 	public static void main(String[] args) {
-		System.out.println("Hello Manthan how r u");
+//		System.out.println("Hello Manthan how r u");
+		System.out.println("Hello I'm Fine");
 	}
 }
